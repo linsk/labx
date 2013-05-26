968049c1da090f99a34ced8e208ae660afea4f41
linsk <sk@mothin.com>
2013-05-26 11:56:07 +0800

    notification 调整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 	def display_notice_and_alert
     msg = ''
-    msg << (content_tag :div, notice, :class => "alert alert-info") if notice
-    msg << (content_tag :div, alert, :class => "alert alert-error") if alert
+    msg << (content_tag :div, notice, :class => "alert alert-info mobile_alert") if notice
+    msg << (content_tag :div, alert, :class => "alert alert-error mobile_alert") if alert
     sanitize msg
   end
 end
